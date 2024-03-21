3a19850ff2b28b0f90ed85231b1895d9afda1399
ananyaraavi <ananyaraavi03@gmail.com>
2024-03-21 23:17:47 +0530

    Added navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import {motion} from 'framer-motion';    //animations
+import {styles} from '../styles'
+import {ComputersCanvas} from './canvas';
+
+const Hero = () => {
+  return (
+    <section>Hero</section>
+  )
+}
+
+export default Hero
